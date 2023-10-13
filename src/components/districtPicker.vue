f3af30e4bd70f07bb73b565952f691dc43ce10d5
Izzudin Ar Rafiq <izzudin.ar26@gmail.com>
2023-10-14 02:37:57 +0700

    fixing mobile layout problem

--- src/components/districtPicker.vue
+++ src/components/districtPicker.vue
@@ -10,12 +10,12 @@ const { districts, selectedDistrict, showDistrict } = storeToRefs(stunting)
     <Transition>
         <div v-if="showDistrict"
             class="fixed bg-black/50 inset-0 z-50 w-[100svw] h-[100svh] items-center justify-center flex">
-            <div class="bg-white rounded-lg w-11/12 overflow-auto md:w-1/2 lg:w-1/3">
+            <div class="bg-white rounded-lg w-11/12 overflow-auto md:w-1/2 max-h-[80svh] lg:w-1/3">
                 <div class="flex flex-col p-5">
                     <div class="flex flex-row">
                         <h1 class="text-xl font-semibold">Pilih Kecamatan</h1>
                     </div>
-                    <div class="grid grid-cols-2 gap-3 mt-5">
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-3 mt-5">
                         <div v-for="(d, i) in districts" :key="i" @click="stunting.selectDistrict(i)"
                             :class="[i == selectedDistrict && 'bg-primary text-white', 'px-3 py-1 ring-secondary text-secondary font-semibold ring-1 rounded-md']">
                             {{ d.district }}
